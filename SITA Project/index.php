07050ff4bffe8a1a437d15fcb41256a71261d346
Tomson Pangaribuan <tomsonpangaribuan@gmail.com>
2017-06-30 10:01:58 +0700

    Ganti Nama Page
    Login.php >> Index.php
    Index.php >> Home.php

--- SITA Project/index.php
+++ SITA Project/index.php
@@ -1,116 +1,173 @@
 <!DOCTYPE html>
-<!-- Website template by freewebsitetemplates.com -->
-<html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-		<title>SITA Project</title>
-		<link rel="stylesheet" href="css/style.css" type="text/css" charset="utf-8" />
-		<link rel="icon" href="images/favicon.png">
-	</head>
-	
-	<body>
-		<div class=navi>
-			<ul>
-				<img src="images/bgnav.png" alt="">
-				<li><a href="default.asp">Home</a></li>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>SITA Project Login</title>
+  	<link rel="icon" href="images/favicon.png">
+  
+  
+      <style>
+      /* NOTE: The styles were added inline because Prefixfree needs access to your styles and they must be inlined if they are on local disk! */
+      @import url(https://fonts.googleapis.com/css?family=Exo:100,200,400);
+@import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:700,400,300);
 
-				<li class="dropdown">
-					<a href="default.asp" class="dropbtn">Data Site</a>
-					<div class="dropdown-content">
-	      				<a href="#">Link 1</a>
-					    <a href="#">Link 2</a>
-					    <a href="#">Link 3</a>
-				    </div>
-				</li>
+body{
+	margin: 0;
+	padding: 0;
+	background: #fff;
 
-				<li><a href="contact.asp">Administrasi</a></li>
-				<li><a href="about.asp">About</a></li>
+	color: #fff;
+	font-family: Arial;
+	font-size: 12px;
+}
 
-				<form action="" id="search">
-					<input type="text"/>
-					<input type="submit" value="" id="submit"/>
-				</form>
-			</ul>
+.body{
+	position: absolute;
+	top: -20px;
+	left: -20px;
+	right: -40px;
+	bottom: -40px;
+	width: auto;
+	height: auto;
+	background-image: url(images/bg.jpg);
+	background-size: cover;
+	-webkit-filter: blur(5px);
+	z-index: 0;
+}
 
-			
+.grad{
+	position: absolute;
+	top: -20px;
+	left: -20px;
+	right: -40px;
+	bottom: -40px;
+	width: auto;
+	height: auto;
+	background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0)), color-stop(100%,rgba(0,0,0,0.65))); /* Chrome,Safari4+ */
+	z-index: 1;
+	opacity: 0.7;
+}
+
+.header{
+	position: absolute;
+	top: calc(50% - 35px);
+	left: calc(50% - 255px);
+	z-index: 2;
+}
+
+.header div{
+	float: left;
+	color: #fff;
+	font-family: 'Exo', sans-serif;
+	font-size: 35px;
+	font-weight: 200;
+}
+
+.header div span{
+	color: #5379fa !important;
+}
+
+.login{
+	position: absolute;
+	top: calc(50% - 75px);
+	left: calc(50% - 50px);
+	height: 150px;
+	width: 350px;
+	padding: 10px;
+	z-index: 2;
+}
+
+.login input[type=text]{
+	width: 250px;
+	height: 30px;
+	background: transparent;
+	border: 1px solid rgba(255,255,255,0.6);
+	border-radius: 2px;
+	color: #fff;
+	font-family: 'Exo', sans-serif;
+	font-size: 16px;
+	font-weight: 400;
+	padding: 4px;
+}
+
+.login input[type=password]{
+	width: 250px;
+	height: 30px;
+	background: transparent;
+	border: 1px solid rgba(255,255,255,0.6);
+	border-radius: 2px;
+	color: #fff;
+	font-family: 'Exo', sans-serif;
+	font-size: 16px;
+	font-weight: 400;
+	padding: 4px;
+	margin-top: 10px;
+}
+
+.login input[type=button]{
+	width: 260px;
+	height: 35px;
+	background: #fff;
+	border: 1px solid #fff;
+	cursor: pointer;
+	border-radius: 2px;
+	color: #a18d6c;
+	font-family: 'Exo', sans-serif;
+	font-size: 16px;
+	font-weight: 400;
+	padding: 6px;
+	margin-top: 10px;
+}
+
+.login input[type=button]:hover{
+	opacity: 0.8;
+}
+
+.login input[type=button]:active{
+	opacity: 0.6;
+}
+
+.login input[type=text]:focus{
+	outline: none;
+	border: 1px solid rgba(255,255,255,0.9);
+}
+
+.login input[type=password]:focus{
+	outline: none;
+	border: 1px solid rgba(255,255,255,0.9);
+}
+
+.login input[type=button]:focus{
+	outline: none;
+}
+
+::-webkit-input-placeholder{
+   color: rgba(255,255,255,0.6);
+}
+
+::-moz-input-placeholder{
+   color: rgba(255,255,255,0.6);
+}
+    </style>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+
+</head>
+
+<body>
+  <div class="body"></div>
+		<div class="grad"></div>
+		<div class="header">
+			<div>SITA<span>Project</span></div>
 		</div>
-		<div id="background">
-			<img src="images/bg1.jpg" alt="abs-img" class="abs-img" />
-
-			<div class="page">
-				<img src="images/bg3.png" alt="abs3-img" class="abs3-img" />
-				<img src="images/bg4.png" alt="abs4-img" class="abs4-img" />
-				<div class="sidebar">
-
-					<div class="featured">						
-						<a href="gallery.php" class="figure"><img src="images/person.jpg" alt=""/></a>		
-					</div>
-
-					<div id="tweets">
-						<h3>Staff 1</h3>
-						<p>
-							<br>Nama User : Immanuel Rio
-							<br>NIK : 19574
-							<br>Jabatan : Staff Manajemen Site
-							<br>Akun : Admin
-						</p>
-					</div>
-					<div id="article">
-						<h3>MENU ADMIN</h3>
-						<p>
-							<br>ADD DATA<br>
-							<br>UPDATE DATA<br>
-							<br>DELETE DATA<br>
-						</p>
-					</div>
-					
-					<p>&#169; Copyright 2017. Created by Tomson Pangaribuan</p>
-				</div>
-				<div class="body">
-				<img src="images/bg2.png" alt="ab2s-img" class="
-				abs2-img" />
-
-					<h1><a href="index.php">Selamat Datang Di SITA Project</a></h1>
-
-					<ul class="navigation">
-						<li><a href="blog.php">Berita</a></li>
-						<li><a href="accessories.php">RAB PBB</a></li>
-						<li><a href="tutorials.php">RAB SKRD</a></li>
-						<li><a href="guide.php">Perizinan</a></li>
-						<li><a href="gallery.php">Perpanjangan</a></li>
-					</ul>
-
-					<div id="featured">
-						<div>
-							<h3>Pengenalan Terhadap Web ini :
-							<p>Web ini adalah tempat penampungan dan pengolahan data site untuk kemudian data dapat dipilah sesuai dengan kebutuhan pengguna, web ini juga menyediakan fitur untuk melakukan print tabel kedalam bentuk dokumen.
-							<br>Ada dua hak akses pada web ini : 
-							<br>1. Admin (Hampir dapat melakukan semua perubahan pada web / God-like)
-							<br>2. User (Pengguna akhir yang hanya dapat melakukan fungsi view, search dan print).
-							
-							<br><br>
-							Bila ada pertanyaan yang ingin diajukan, segera hubungi kami melalui link berikut ini. 
-							<a href="About.php">Hubungi kami !</a>.</p>
-
-							<h3>Q & A (Pertanyaan Umum):
-							<p>1.Saya Lupa Pasword / User Name saya, Apa yang dapat saya lakukan?
-							<br>Hubungi Admin secara langsung atau Klik Halaman "About" untuk melihat informasi kontak Admin.
-							<br>
-							<br>2.Saya ingin mengubah data diri saya
-							<br>Hubungi Admin secara langsung atau Klik Halaman "About" untuk melihat informasi kontak Admin.
-							<br>
-							<br>3.Siapa saja yang bisa melakukan insert / delete data site dan data lain pada web ini?
-							<br>Admin
-							<br>
-							<br>4.Web ini menolong sekali dan bertampilan bagus, apa yang dapat saya berikan?
-							<br>Tolong Saya dikasih Nilai A . :)
-						</div>
-	
-					</div>
-				
-					
-				</div>
-			</div>
+		<br>
+		<div  class="login" >
+				<input type="text" placeholder="username" name="user"><br>
+				<input type="password" placeholder="password" name="password"><br>
+				<input  type="button" onclick="location.href='Home.php';" value="Login" >
 		</div>
-	</body>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+  
+</body>
 </html>
