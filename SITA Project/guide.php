504db57a63b8a9f341f278db008f7497fc6e1963
Tomson Pangaribuan <tomsonpangaribuan@gmail.com>
2017-06-30 08:29:32 +0700

    Add files via upload

---
+++ SITA Project/guide.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<title>Photography Template</title>
+		<link rel="stylesheet" href="css/style.css" type="text/css" charset="utf-8" />
+	</head>
+	
+	<body>
+		<div id="background">
+			<img src="images/absolute-img.jpg" alt="abs-img" class="abs-img" />
+			<div class="page">
+				<div class="sidebar">
+					<div class="featured">
+						<h3>Featured Photo</h3>
+						<a href="gallery.php" class="figure"><img src="images/butterfly.jpg" alt=""/></a>
+						<a href="gallery.php">preview</a>
+						<span>by <font>H.U</font></span>
+						<a href="blog.php" class="comments">12</a>
+					</div>
+					<div id="tweets">
+						<h3>Tweets</h3>
+						<p>This website template has been designed by <a href="http://www.freewebsitetemplates.com/">Free Website Templates</a> for you, for free.</p>
+					</div>
+					<div id="article">
+						<h3>Discussions</h3>
+						<ul>
+							<li>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+								<span>Sept 21 | by <a href="blog.php">Nullam</a></span>
+								<a href="blog.php">8</a>
+							</li>
+							<li>
+								<p>Donec porttitor eros eget diam lobortis tincidunt.</p>
+								<span>Sept 21 | by <a href="blog.php">Nullam</a></span>
+								<a href="blog.php">8</a>
+							</li>
+							<li>
+								<p>Pellentesque et libero a urna molestie sagittis sit amet</p>
+								<span>Sept 21 | by <a href="blog.php">Nullam</a></span>
+								<a href="blog.php">8</a>
+							</li>
+							<li>
+								<p>Aliquam erat volutpat. Maecenas risus mauris</p>
+								<span>Sept 21 | by <a href="blog.php">Nullam</a></span>
+								<a href="blog.php">8</a>
+							</li>
+								<li>
+								<p>Vivamus tincidunt urna id nisi malesuada vehicula</p>
+								<span>Sept 21 | by <a href="blog.php">Nullam</a></span>
+								<a href="blog.php">8</a>
+							</li>
+						</ul>
+					</div>
+					<div id="connect">
+						<h2>Stay Informed:</h2>
+						<a href="http://facebook.com/freewebsitetemplates" class="facebook">Facebook</a>
+						<a href="http://twitter.com/fwtemplates" class="twitter">Twitter</a>
+						<form action="">
+							<input type="text"/>
+							<input type="submit" class="submit" value=""/>
+						</form>
+					</div>
+					<p>&#169; Copyright 2012. All Rights Reserved.</p>
+				</div>
+				<div class="body">
+					<ul id="navigation">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="about.php">About</a></li>
+						<li><a href="contact.php">Contact Us</a></li>
+					</ul>
+					<form action="" id="search">
+						<input type="text"/>
+						<input type="submit" value="" id="submit"/>
+					</form>
+					<h1><a href="index.php">Photographers Daily Dose</a></h1>
+					<ul class="navigation">
+						<li><a href="blog.php">Articles &amp; </br> Blog</a></li>
+						<li><a href="accessories.php">Cameras &amp; </br> Accessories</a></li>
+						<li><a href="tutorials.php">How To </br> &amp; Tutorials</a></li>
+						<li class="selected"><a href="guide.php">Buying </br> Guide</a></li>
+						<li><a href="gallery.php">Photo </br> Gallery</a></li>
+					</ul>
+					<div class="guide">
+						<h1>Buying Guide</h1>
+						<h4>Looking for more templates? Just browse through all our <a href="http://www.freewebsitetemplates.com/">Free Website Templates</a> and find what you're looking for.</h4>
+						<p>But if you don't find any website template you can use, you can try our <a href="http://www.freewebsitetemplates.com/freewebdesign/">Free Web Design</a> service and tell us all about it. 
+						Maybe you're looking for something different, something special. And we love the challenge of doing something different and something special.</p>
+						<p>You can remove any link to our website from this website template, you're free to use this website template without linking back to us.</p>
+						<div>
+							<img src="images/dslr-camera.jpg" alt=""/>
+							<ul>
+								<li>
+									<p>Cras  varius eget varius atplacerat tristique tempus vitae, malesuada nec leo. Quisque ac turpis enim, a rutrum nunc.</p>
+								</li>
+								<li>
+									<p>Nulla mollis massa ut felis semper luctus. Donec consequat arcu eget sapien scelerisque a placerat nisi cursus.</p>
+								</li>
+									<li>
+									<p>Suspen tellus qudisse aenean sit amet enim et eblandit augue vel pulvinar. Vivamus luctus aliquam massa eu luctus. </p>
+								</li>
+							</ul>
+						</div>
+						<p>Quisque facilisis turpis vel elit rutrum tempor. Etiam vestibulum sapien eget felis fringilla lobortis. Morbi ultrices, dui vitae malesuada mollis, nisl erat 
+						vehicula risus, vel imperdiet justo risus sed lacus. Pellentesque nec velit mauris, vel rutrum massa. Pellentesque pulvinar mattis posuere. Maecenas luctus 
+						tortor eget ante facilisis id egestas tortor tempor. Pellentesque sem justo, varius eget varius at, vehicula vel tellus. In accumsan ipsum quis justo pharetra 
+						id malesuada eros euismod. Maecenas nisi ipsum, aliquam et adipiscing sit amet, tempor eget eros. Sed turpis metus, malesuada sed dictum non, posuere 
+						quis diam. Cras sit amet libero ac dui ultricies auctor eu ac risus.</p>
+						<p>This website template has been designed by <a href="http://www.freewebsitetemplates.com/">Free Website Templates</a> for you, for free. 
+						You can replace all this text with your own text. You can remove any link to our website from this website template, you're free to use this 
+						website template without linking back to us. If you're having problems editing this website template, then don't hesitate to ask for help on 
+						the <a href="http://www.freewebsitetemplates.com/forum/">Forum</a>.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ SITA Project/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<title>SITA Project</title>
+		<link rel="stylesheet" href="css/style.css" type="text/css" charset="utf-8" />
+		<link rel="icon" href="images/favicon.png">
+	</head>
+	
+	<body>
+		<div class=navi>
+			<ul>
+				<img src="images/bgnav.png" alt="">
+				<li><a href="default.asp">Home</a></li>
+				<li><a href="news.asp">News</a></li>
+				<li><a href="contact.asp">Contact</a></li>
+				<li><a href="about.asp">About</a></li>
+
+				<form action="" id="search">
+					<input type="text"/>
+					<input type="submit" value="" id="submit"/>
+				</form>
+			</ul>
+
+			
+		</div>
+		<div id="background">
+			<img src="images/bg1.jpg" alt="abs-img" class="abs-img" />
+
+			<div class="page">
+				<img src="images/bg3.png" alt="abs3-img" class="abs3-img" />
+				<img src="images/bg4.png" alt="abs4-img" class="abs4-img" />
+				<div class="sidebar">
+
+					<div class="featured">						
+						<a href="gallery.php" class="figure"><img src="images/person.jpg" alt=""/></a>		
+					</div>
+
+					<div id="tweets">
+						<h3>Staff 1</h3>
+						<p>
+							<br>Nama User : Mr.Incredible
+							<br>NIK : 19574
+							<br>Jabatan : Staff Manajemen WEB
+							<br>Akun : User / Admin
+						</p>
+					</div>
+					<div id="article">
+						<h3>MENU ADMIN</h3>
+						<p>
+							<br>ADD DATA<br>
+							<br>UPDATE DATA<br>
+							<br>DELETE DATA<br>
+						</p>
+					</div>
+					
+					<p>&#169; Copyright 2017. Created by Tomson Pangaribuan</p>
+				</div>
+				<div class="body">
+				<img src="images/bg2.png" alt="ab2s-img" class="
+				abs2-img" />
+
+					<h1><a href="index.php">Selamat Datang Di SITA Project</a></h1>
+
+					<ul class="navigation">
+						<li><a href="blog.php">Berita</a></li>
+						<li><a href="accessories.php">RAB PBB</a></li>
+						<li><a href="tutorials.php">RAB SKRD</a></li>
+						<li><a href="guide.php">Perizinan</a></li>
+						<li><a href="gallery.php">Perpanjangan</a></li>
+					</ul>
+
+					<div id="featured">
+						<div>
+							<h3>Pengenalan Terhadap Web ini :
+							<p>Web ini adalah tempat penampungan dan pengolahan data site untuk kemudian data dapat dipilah sesuai dengan kebutuhan pengguna, web ini juga menyediakan fitur untuk melakukan print tabel kedalam bentuk dokumen.
+							<br>Ada dua hak akses pada web ini : 
+							<br>1. Admin (Hampir dapat melakukan semua perubahan pada web / God-like)
+							<br>2. User (Pengguna akhir yang hanya dapat melakukan fungsi view, search dan print).
+							
+							<br><br>
+							Bila ada pertanyaan yang ingin diajukan, segera hubungi kami melalui link berikut ini. 
+							<a href="About.php">Hubungi kami !</a>.</p>
+
+							<h3>Q & A (Pertanyaan Umum):
+							<p>1.Saya Lupa Pasword / User Name saya, Apa yang dapat saya lakukan?
+							<br>Hubungi Admin secara langsung atau Klik Halaman "About" untuk melihat informasi kontak Admin.
+							<br>
+							<br>2.Saya ingin mengubah data diri saya
+							<br>Hubungi Admin secara langsung atau Klik Halaman "About" untuk melihat informasi kontak Admin.
+							<br>
+							<br>3.Siapa saja yang bisa melakukan insert / delete data site dan data lain pada web ini?
+							<br>Admin
+							<br>
+							<br>4.Web ini menolong sekali dan bertampilan bagus, apa yang dapat saya berikan?
+							<br>Tolong Saya dikasih Nilai A . :)
+						</div>
+	
+					</div>
+				
+					
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
