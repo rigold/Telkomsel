9b99ccb847ed2b035b60e4b8e09f3a5ed4bd35c5
Tomson Pangaribuan <tomsonpangaribuan@gmail.com>
2017-07-02 23:28:19 +0700

    Perbaikan Paragraf

---
+++ SITA Project/About.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<title>SITA Project</title>
+		<link rel="stylesheet" href="css/style.css" type="text/css" charset="utf-8" />
+		<link rel="icon" href="images/favicon.png">
+	</head>
+	
+	<body>
+		<div class=navi>
+			<ul>
+				<img src="images/bgnav.png" alt="">
+				<li><a href="Home.php">Home</a></li>
+
+				<li class="dropdown">
+					<a class="dropbtn">Data Site</a>
+					<div class="dropdown-content">
+	      				<a href="#"> Bangkalan </a> 
+						<a href="#"> Banyuwangi </a> 
+						<a href="#"> Batu </a> 
+						<a href="#"> Blitar Kab. </a> 
+						<a href="#"> Blitar Kota </a> 
+						<a href="#"> Bojonegoro </a> 
+						<a href="#"> Bondowoso </a> 
+						<a href="#"> Gresik </a> 
+						<a href="#"> Jember </a> 
+						<a href="#"> Jombang </a> 
+						<a href="#"> Kediri Kab. </a> 
+						<a href="#"> Kediri Kota </a> 
+						<a href="#"> Lamongan </a> 
+						<a href="#"> Lumajang </a> 
+						<a href="#"> Madiun Kab. </a> 
+						<a href="#"> Madiun Kota </a> 
+						<a href="#"> Magetan </a> 
+						<a href="#"> Malang Kab. </a> 
+						<a href="#"> Malang Kota </a> 
+						<a href="#"> Mojokerto Kab. </a> 
+						<a href="#"> Mojokerto Kota </a> 
+						<a href="#"> Nganjuk </a> 
+						<a href="#"> Ngawi </a> 
+						<a href="#"> Pacitan </a> 
+						<a href="#"> Pamekasan </a> 
+						<a href="#"> Pasuruan Kab. </a> 
+						<a href="#"> Pasuruan Kota </a> 
+						<a href="#"> Ponorogo </a> 
+						<a href="#"> Probolinggo Kab. </a> 
+						<a href="#"> Probolinggo Kota </a> 
+						<a href="#"> Sampang </a> 
+						<a href="#"> Sidoarjo </a> 
+						<a href="#"> Situbondo </a> 
+						<a href="#"> Sumenep </a> 
+						<a href="#"> Surabaya </a> 
+						<a href="#"> Trenggalek </a> 
+						<a href="#"> Tuban </a> 
+						<a href="#"> Tulungagung </a> 
+
+				    </div>
+				</li>
+
+				<li class="dropdown">
+					<a class="dropbtn">Administrasi</a>
+					<div class="dropdown-content">
+	      				<a href="List_Panjang.php">List Perpanjangan</a>
+	      				<a href="Monitoring.php">Monitoring</a>
+					    <a href="PBB.php">PBB</a>
+					    <a href="RPM.php">RPM</a>
+					    <a href="Comcase.php">Comcase</a>
+				    </div>
+				</li>
+				<li><a href="About.php">About</a></li>
+				<li><a href="Index.php">Log out</a></li>
+
+				<form action="Search.php" id="search">
+					<input type="text"/>
+					<input type="submit" onclick="location.href='Search.php';" value="" id="submit"/>
+				</form>
+			</ul>	
+		</div>
+
+		<div id="background">
+			<img src="images/bg1.jpg" alt="abs-img" class="abs-img" />
+
+			<div class="page">
+				<img src="images/bg3.png" alt="abs3-img" class="abs3-img" />
+				<img src="images/bg4.png" alt="abs4-img" class="abs4-img" />
+				<div class="sidebar">
+
+					<div class="featured">						
+						<a href="gallery.php" class="figure"><img src="images/person.jpg" alt=""/></a>		
+					</div>
+
+					<div id="tweets">
+						<h3>Staff 1</h3>
+						<p>
+							<br>Nama User : Immanuel Rio
+							<br>NIK : 19574
+							<br>Jabatan : Staff Manajemen Site
+							<br>Akun : Admin
+						</p>
+					</div>
+					<div id="article">
+						<h3>MENU ADMIN</h3>
+						<p>
+							<a href="Add_User.php">> ADD USER<br></a>
+							<a href="Add_Sites.php"><br>> ADD SITE<br></a>
+							<a href="Edit_User.php"><br>> EDIT USER<br></a>
+							<a href="Edit_Sites.php"><br>> EDIT SITE<br></a>
+							<a href="List_User.php"><br>> LIST USER<br></a>
+							<a href="List_Sites.php"><br>> LIST SITES<br></a>
+						</p>
+					</div>
+					
+					<p><br><br>&#169; Copyright 2017. Created by Rigold Nainggolan & Tomson Pangaribuan</p>
+				</div>
+
+				<div class="body">
+					<img src="images/bg2.png" alt="ab2s-img" class="
+					abs2-img" />
+
+					<h2><a href="About.php">ABOUT - Tentang Kami</a></h2>
+
+					<div id="featured">
+						<div>
+							<h3>Pengenalan Terhadap Web ini :
+								<p>
+									Web ini adalah tempat penampungan dan pengolahan data site untuk kemudian data dapat dipilah sesuai dengan kebutuhan pengguna, web ini juga menyediakan fitur untuk melakukan print tabel kedalam bentuk dokumen.
+									<br>Ada dua hak akses pada web ini :<br> 
+									<br>1. Admin (Hampir dapat melakukan semua perubahan pada web / God-like)
+									<br>2. User (Pengguna akhir yang hanya dapat melakukan fungsi view, search dan print).
+									
+									<br><br>
+									Bila ada pertanyaan yang ingin diajukan, segera hubungi kami melalui link berikut ini. 
+									<a href="About.php">Hubungi kami !</a>
+								</p>
+							</h3>
+							<h3>Q & A (Pertanyaan Umum):
+								<p>
+									1.Saya Lupa Pasword / User Name saya, Apa yang dapat saya lakukan?
+									<br>Hubungi Admin secara langsung atau Klik Halaman "About" untuk melihat informasi kontak Admin.
+									<br>
+									<br>2.Saya ingin mengubah data diri saya
+									<br>Hubungi Admin secara langsung atau Klik Halaman "About" untuk melihat informasi kontak Admin.
+									<br>
+									<br>3.Siapa saja yang bisa melakukan insert / delete data site dan data lain pada web ini?
+									<br>Admin
+									<br>
+									<br>4.Web ini menolong sekali dan bertampilan bagus, apa yang dapat saya berikan?
+									<br>Tolong Kami dikasih Nilai A . :)
+								</p>
+							</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
