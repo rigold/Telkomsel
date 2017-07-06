5f960b6027949ebdf6722c9560b1edecdf6c65c6
rigold <rigold95@gmail.com>
2017-07-06 09:49:22 +0700

    conect

---
+++ SITA Project/connect.php
@@ -0,0 +1,17 @@
+<?php
+$servername ="localhost";
+$username = "root";
+$password = "";
+$database = "sita";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if(!$conn) {
+	echo "failed";
+	die("Connection failed: " . mysqli_connect_error());	
+}
+else {
+	echo "success";
+}
+
+?>
