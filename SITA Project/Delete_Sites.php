5de9a3b156433b9498f76f0117b72619ce1631b7
rigold <rigold95@gmail.com>
2017-07-19 11:29:24 +0700

    delete

--- SITA Project/Delete_Sites.php
+++ SITA Project/Delete_Sites.php
@@ -8,7 +8,7 @@
 
 
 	$sites_id = $_GET['sites_id'];
-	$result = mysqli_query($conn, "DELETE FROM site WHERE sites_id=$sites_id");
+	$result = mysqli_query($conn, "DELETE FROM site WHERE sites_id='$sites_id'");
 	 
 	$file1 = $_GET['file1'];
 	unlink($file1);
